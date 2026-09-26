2346fc04c69eafc0db5fdfbdb15d3d1311a4520c
Jay-ar S. De Guzman <jrsaturno66@gmail.com>
2026-09-27 00:14:43 +0800

    added some features in email notifications

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', "CED Registrar's E-Services"),
 
     /*
     |--------------------------------------------------------------------------
